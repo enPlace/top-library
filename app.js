5d4204fd1dbef82a9d067ac24a13f34089b57fca
enPlace <n.place@protonmail.com>
2021-06-18 16:13:22 -0300

    basic book object

---
+++ app.js
@@ -0,0 +1,15 @@
+const Book = function(name, pages, read=false){
+    this.name = name
+    this.pages = pages
+    this.read = read
+}
+
+Book.prototype.print= function(){
+    console.log(
+        this.name, this.pages, this.read
+    )
+}
+Book.prototype.changeStatus = function(){
+    this.read = true
+}
+const Harry = new Book("harry potter", 300)
