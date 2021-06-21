20a68aea30bca20a7e46ee3775c611d27be19b53
enPlace <n.place@protonmail.com>
2021-06-21 15:54:53 -0300

    books api search

---
+++ books-api.js
@@ -0,0 +1,13 @@
+//for using the google books api
+
+const bookSearch = async function(searchText){
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${searchText}`
+    const response = await fetch(url)
+    if(!response.ok){
+        throw Error(response.status)
+    }
+    const data = await response.json()
+    
+    return data
+
+}
