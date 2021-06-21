f29788db596766460dabcbf5005f9fa03cc6d42c
enPlace <n.place@protonmail.com>
2021-06-21 05:53:19 -0300

    updating default library

--- scripts/app.js
+++ scripts/app.js
@@ -5,31 +5,50 @@ let closeButtons = document.querySelectorAll(".close")
 let userResults = document.getElementById("user-results")
 const defaultLibrary= {
     book1:{
-        name: "Harry Potter",
-        author: "JK Rowling",
+        name: "The Philosophy of Jean-Paul Sartre",
+        author: "Jean-Paul Sartre",
         read: "true",
-        isbn: "kcnkdlimce",
+        isbn: "1400076323",
     }, 
     book2:{
-        name: "Where's Waldo",
-        author: "unknown",
-        read:"false",
-        isbn: "ldmice",
+        name: "Boggs:A Comedy of Values",
+        author: "Lawrence Weschler  ",
+        read:"true",
+        isbn: "9780226893969",
     },
     book3:{
-        name: "The Stranger",
-        author: "albert camus",
+        name: "Ficciones",
+        author: "Jorge Louis Borges",
         read: "true",
-        isbn: "kcmiekd",
+        isbn: "8426405738",
+    }, 
+    book4:{
+        name: "Something Deeply Hidden: Quantum Worlds and the Emergence of Spacetime",
+        author: "Sean Carrol",
+        read: "false",
+        isbn: "9781524743017",
+    }, 
+    book5:{
+        name: "Gödel, Escher, Bach: An Eternal Golden Braid",
+        author: "Douglas Hofstadter",
+        read: "true",
+        isbn: "0465026567"
+},
+    book6:{
+        name: "Cracking the Coding Interview", 
+        author: "Gale Laakmann McDowell",
+        read: "false",
+        isbn: "0984782850",
     }};
 
 class Book{
-    constructor(name, author, isbn=false, read=false){
+    constructor(name, author, isbn=false, read=false, imgsrc){
     this.name = name
     this.author = author
     this.isbn = isbn
     this.read = read
     this.dateAdded = new Date()
+    this.imgsrc = imgsrc
     }
     print(){
         console.log(this.name, this.pages, this.read)
@@ -43,10 +62,10 @@ function newBookObject(name, author, isbn, read){
     myLibrary[booknum] = new Book(name, author, isbn, read)
     booknum = (parseInt(booknum) +1).toString()
 }
-function newCard(){
+function newCard(name, author, isbn, read, imgsrc){
     let newcard = document.createElement('div')
     newcard.classList = "user-card"
-    newcard.innerHTML = '<div class="close"><button>&times;</button></div><img src="images.jpg" class="avatar"> <div class="top info">FirstName </div><div class="bottom info">LastName </div>'
+    newcard.innerHTML = `<div class="close"><button>&times;</button></div><img src="images.jpg" class="avatar"> <div class="top info">${name} </div><div class="bottom info">${author} </div>`
     userResults.appendChild(newcard)
 
     
