11ff2429743e693593ca68273f9ee1b3d52e6bae
enPlace <n.place@protonmail.com>
2021-06-21 21:06:32 -0300

    add card from search

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,7 @@ const addBookButton = document.getElementById("add-book-button")
 //const keys = Object.keys(defaultLibrary)
 
 class Book{
-    constructor(id, name, author, read=false, isbn=false, imgsrc){
+    constructor(id, name, author, isbn, read, imgsrc){
     this.id = id
     this.name = name
     this.author = author
@@ -25,8 +25,8 @@ class Book{
     }    
 }
 
-function newBookObject(name, author, isbn, read){
-    myLibrary[booknum] = new Book(name, author, isbn, read)
+function newBookObject(name, author, isbn, read, imgsrc){
+    myLibrary[booknum] = new Book(booknum, name, author, isbn, read, imgsrc)
     booknum = (parseInt(booknum) +1).toString()
 }
 function newCard(book){
@@ -56,29 +56,46 @@ function loadLibrary(library){
     })
 }
 let resultados
-addBookButton.addEventListener('click',async (e)=>{
-    e.preventDefault()
-    let hasread
-    if(newBookForm.cbox1.checked){hasread = true}
-    let title = newBookForm.title.value
-    let author = newBookForm.title.value 
-    let isbn = newBookForm.isbn.value
-    
-    results = await bookSearch(title, author, isbn)
-    console.log(results)
+let currentTitle
+let currentAuthor
+let currentISBN
+let currentImgsrc
+let hasread
+async function populateSearchModal(title, author, isbn){
+    results = await bookSearch(`${title} ${author} ${isbn}`)
     resultados = results
     booksearchImg.innerHTML = `<img src=${results.items[0].volumeInfo.imageLinks.thumbnail} alt=""></img>`
     booksearchTitle.innerHTML = results.items[0].volumeInfo.title
     booksearchAuthor.innerHTML = results.items[0].volumeInfo.authors
     booksearchISBN.innerHTML = `ISBN-10: ${results.items[0].volumeInfo.industryIdentifiers[1].identifier}`
+    currentTitle = results.items[0].volumeInfo.title
+    currentAuthor = results.items[0].volumeInfo.authors
+    currentISBN = results.items[0].volumeInfo.industryIdentifiers[1].identifier
+    currentImgsrc = results.items[0].volumeInfo.imageLinks.thumbnail
+}
+addBookButton.addEventListener('click',async (e)=>{
+    e.preventDefault()
+    if(newBookForm.cbox1.checked){hasread = true}else{hasread = false}
+    let title= newBookForm.title.value
+    let author = newBookForm.author.value 
+    let isbn = newBookForm.isbn.value
+    await populateSearchModal(title, author, isbn)
     
     toggleBookModal()
     toggleBookModal(booksearchModal)
-    //myLibrary[booknum] = new Book (booknum, newBookForm.title.value, newBookForm.author.value, hasread)
-    //newCard(myLibrary[booknum])
-    //booknum = (parseInt(booknum)+1).toString()
-    //console.log(myLibrary)
-    //saveUserLibrary()
+})
+
+const confirmButton = document.getElementById("confirm")
+confirmButton.addEventListener('click', (e)=>{
+    newBookObject(currentTitle, currentAuthor, currentISBN, hasread, currentImgsrc )
+    saveUserLibrary()
+
+
+
+    /* function newBookObject(name, author, isbn, read, imgsrc){
+    myLibrary[booknum] = new Book(name, author, isbn, read, imgsrc)
+    booknum = (parseInt(booknum) +1).toString() */
+    
 })
 
 
