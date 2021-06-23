347bb02a6d4b39e628a4265a5eef9c42d79c995f
enPlace <n.place@protonmail.com>
2021-06-22 21:53:24 -0300

    remove children

--- README.md
+++ README.md
@@ -1,24 +1,9 @@
 # top-library
 
-I'll have the form, which creates the object. The object will be put into an array that has a key. 
-for instance: 
-{key1: {object1}, key2, {object2}}
 
-the object will have the following properties: 
+up next: 
+make some kind of identifier that allows the user to click the book
+which adds the book to the list
 
-    book1:{
-        name: "Harry Potter",
-        author: "JK Rowling",
-        read: "true",
-        isbn: "kcnkdlimce",
-        dateCreated: datetime string
-    }, 
-
-now when I add a book, what do I want to do? 
-
-I want to send the search to google books api, then upon the response, pop up another modal that asks "is this your book?"
-
-if yes, a new book is created, if no, the next book in the volume collection is shown, and a button that says "continue without using search data" or something 
-
-When the user clicks yes, the data from the api response automatically makes a new book object and populates the page 
-
+It should probably go back to the first modal with the selected 
+book as the new data, to allow the user to confirm the selction. 
--- book-list.js
+++ book-list.js
@@ -11,15 +11,25 @@ getSample()
 
 const bookResultsContainer = document.getElementById("book-results-container")
 function populateResultsList(){
+    removeChildren(bookResultsContainer)
+    let i = 0
     resultados.items.forEach(result=>{
         const newListItemContainer = document.createElement("div")
         newListItemContainer.classList = "list-item-container"
+        newListItemContainer.dataset.target = i
         if(result.volumeInfo.imageLinks){
         newListItemContainer.innerHTML = `<div class="list-item-title">${result.volumeInfo.title}</div><div class="list-item-author">${result.volumeInfo.authors}</div><div class="list-item-thumb"><img src=${result.volumeInfo.imageLinks.smallThumbnail} alt=""></div>`
         }else{
             newListItemContainer.innerHTML = `<div class="list-item-title">${result.volumeInfo.title}</div><div class="list-item-author">${result.volumeInfo.authors}</div><div class="list-item-thumb">No image available</div>`
         }
         bookResultsContainer.appendChild(newListItemContainer)
+        i++
     })
 
+}
+function removeChildren(parent){
+    //removes all children of an element
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild)
+    };
 }
