62457d94bea7c0a074768992837960ebd2c9ce4a
enPlace <n.place@protonmail.com>
2021-06-28 19:56:35 -0300

    more books and list dimensions

--- scripts/storage.js
+++ scripts/storage.js
@@ -46,9 +46,18 @@ function saveUserLibrary(){
 
 const defaultLibrary= [
     {
-        id: "1", 
+        author: ["Robert C. Martin"],
+        dateAdded: "2021-06-28T22:51:35.100Z",
+        id: "6",
+        imgsrc: "http://books.google.com/books/content?id=_i6bDeoCQzsC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        isbn: "9780136083252",
+        name: "Clean Code",
+        read: false
+    },
+    {
+        id: "book1", 
         name: "Civilization and Capitalism, 15th-18th Century, Vol. I", 
-        author: Array(1), 
+        author: ["Fernand Braudel"], 
         read: true, 
         isbn: "0520081145",
         imgsrc: "http://books.google.com/books/content?id=rPgVp3vMOjcC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
@@ -57,14 +66,23 @@ const defaultLibrary= [
     {
         author: ["Ray Bradbury"],
         dateAdded: "2021-06-28T22:30:37.164Z",
-        id: "book8",
+        id: "book2",
         imgsrc: "http://books.google.com/books/content?id=1XUzAAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         isbn: "9780575087330",
         name: "Something Wicked This Way Comes",
         read: false
     },
     {
-        id: "book1",
+        author:[ "Eduardo H. Galeano" ],
+        dateAdded: "2021-06-27T17:31:32.029Z",
+        id: "book3",
+        imgsrc: "http://books.google.com/books/content?id=CWYiDgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        isbn: "9788432316050",
+        name: "Las venas abiertas de América Latina",
+        read: true,
+    },
+    {
+        id: "book4",
         name: "The Philosophy of Jean-Paul Sartre",
         author: ["Jean-Paul Sartre"],
         read: true,
@@ -73,7 +91,7 @@ const defaultLibrary= [
         dateAdded: new Date()
     }, 
     {
-        id:"book2",
+        id:"book5",
         name: "Boggs:A Comedy of Values",
         author: ["Lawrence Weschler"],
         read:true,
@@ -83,7 +101,7 @@ const defaultLibrary= [
 
     },
     {
-        id: "book3",
+        id: "book6",
         name: "Ficciones",
         author: ["Jorge Louis Borges"],
         read: true,
@@ -92,7 +110,7 @@ const defaultLibrary= [
         dateAdded: new Date(),
     }, 
     {
-        id: "book4",
+        id: "book7",
         name: "Something Deeply Hidden: Quantum Worlds and the Emergence of Spacetime",
         author: ["Sean Carroll"],
         read: false,
@@ -102,7 +120,7 @@ const defaultLibrary= [
 
     }, 
     {
-        id: "book5",
+        id: "book8",
         name: "Gödel, Escher, Bach: An Eternal Golden Braid",
         author: ["Douglas Hofstadter"],
         read: true,
@@ -112,7 +130,7 @@ const defaultLibrary= [
 
     },
     {
-        id: "book6",
+        id: "book9",
         name: "Cracking the Coding Interview", 
         author: ["Gale Laakmann McDowell"],
         read: false,
@@ -123,11 +141,20 @@ const defaultLibrary= [
     {
         author: ["James Monaco"],
         dateAdded: "2021-06-26T15:42:06.106Z",
-        id: "book7",
+        id: "book10",
         imgsrc: "http://books.google.com/books/content?id=NCDuAAAAMAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
         isbn: "UOM:39015080886594",
         name: "How to Read a Film",
         read: true,
     },
+    {
+        author: ["Bruce Chatwin"],
+        dateAdded: "2021-06-28T22:51:13.906Z",
+        id: "5",
+        imgsrc: "http://books.google.com/books/content?id=S45xoAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+        isbn: "8499423124",
+        name: "En la Patagonia",
+        read: false
+},
 
 ];
